b45334e7918fde15d0cbf8987d51b6ad74067ad1
Bruno Andrade <119361832+BrunoAnndrade@users.noreply.github.com>
2023-08-24 15:54:58 -0300

    edit ui compose

--- app/src/main/java/com/example/calculadoracombustivel/MainActivity.kt
+++ app/src/main/java/com/example/calculadoracombustivel/MainActivity.kt
@@ -4,12 +4,13 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,37 +19,34 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldColors
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.calculadoracombustivel.Componentes.GradientButton
-import com.example.calculadoracombustivel.ui.theme.AmareloClaro
-import com.example.calculadoracombustivel.ui.theme.CalculadoraCombustivelTheme
-import com.example.calculadoracombustivel.ui.theme.Laranja
-import com.example.calculadoracombustivel.ui.theme.Marrom
-import com.example.calculadoracombustivel.ui.theme.RoxoClarinho
+import com.example.calculadoracombustivel.ui.theme.AzulDark
 import com.example.calculadoracombustivel.ui.theme.RoxoClaro
 import com.example.calculadoracombustivel.ui.theme.RoxoEscuro
+import com.example.calculadoracombustivel.ui.theme.ShapeButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,13 +66,13 @@ class MainActivity : ComponentActivity() {
         val resultState = remember { mutableStateOf(0.0) }
 
         Scaffold(topBar = {
-            TopAppBar(backgroundColor = RoxoEscuro,
+            TopAppBar(backgroundColor = AzulDark,
 
                 title = {
                     Text(
                         text = "Calculadora de Combustível",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.ExtraBold,
                         color = Color.White,
                     )
                 })
@@ -95,64 +93,176 @@ class MainActivity : ComponentActivity() {
 
         ) {
 
-
-
             Column(
                 modifier = Modifier
-                    .background(Color.Gray)
+                    .background(Color.White)
                     .fillMaxWidth()
                     .fillMaxHeight(),
                 horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
 
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.Start
 
                 ) {
 
+                    Image(
+                        painter = painterResource(id = R.drawable.fuel_gas02_station),
+                        contentDescription = "Simbolo gasolina",
+                        modifier = Modifier
+                            .padding(end = 10.dp)
+                    )
+
+                    Text(
+                        text = "Consumo (km/L):",
+                        modifier = Modifier
+                            .padding(end = 50.dp)
+                        ,
+                        color = RoxoEscuro,
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    )
+
+                    TextField(
+                        value = consumoVeiculo.take(5),
+                        onValueChange = { consumoVeiculo = it },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        colors = TextFieldDefaults.textFieldColors(
+                            textColor = Color.Red,
+                            backgroundColor = Color.White
+                        ),
+                        modifier = Modifier
+                            .width(70.dp)
+                            .height(50.dp)
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.Start
+
+                ) {
+
+                    Image(
+                        painter = painterResource(id = R.drawable.fuel_gas02_station),
+                        contentDescription = "Simbolo gasolina",
+                        modifier = Modifier
+                            .padding(end = 10.dp)
+
+                    )
+
+                    Text(
+                        text = "Combustível (Litros):",
+                        modifier = Modifier
+                            .padding(end = 9.5.dp),
+                        color = RoxoEscuro,
+                        textAlign = TextAlign.End,
+                        style = TextStyle(
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    )
+
+                    TextField(
+                        value = combustivel.take(5),
+                        onValueChange = { combustivel = it },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        colors = TextFieldDefaults.textFieldColors(
+                            textColor = Color.Red,
+                            backgroundColor = Color.White
+                        ),
+                        modifier = Modifier
+                            .width(70.dp)
+                            .height(50.dp)
+
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.Start
+
+                ) {
+
+                    Image(
+                        painter = painterResource(id = R.drawable.coin_svgrepo_com) ,
+                        contentDescription = "simbolo moeda",
+                        modifier = Modifier
+                            .padding(end = 10.dp)
+                    )
+
+
+                    Text(
+                        text = "Preço por litro R$:",
+                        modifier = Modifier
+                            .padding(end = 40.dp),
+                        color = RoxoEscuro,
+                        textAlign = TextAlign.End,
+                        style = TextStyle(
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    )
+
+                    TextField(
+                        value = precoCombustivel.take(5),
+                        onValueChange = { precoCombustivel = it },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        colors = TextFieldDefaults.textFieldColors(
+                            textColor = Color.Red,
+                            backgroundColor = Color.White
+                        ),
+                        modifier = Modifier
+                            .width(70.dp)
+                            .height(50.dp)
+                    )
+                }
+
                 Spacer(modifier = Modifier.height(16.dp))
 
-                TextField(
-                    value = consumoVeiculo.take(5),
-                    onValueChange = { consumoVeiculo = it },
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                    textStyle = TextStyle(color = Color.Red),
-                    modifier = Modifier
-                        .width(70.dp)
-                        .height(50.dp)
-                )
-                TextField(
-                    value = combustivel.take(5),
-                    onValueChange = { combustivel = it },
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                    textStyle = TextStyle(color = Color.Red),
-                    modifier = Modifier
-                        .width(70.dp)
-                        .height(50.dp)
-                )
-                TextField(
-                    value = precoCombustivel.take(5),
-                    onValueChange = { precoCombustivel = it },
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                    textStyle = TextStyle(color = Color.Red),
-                    modifier = Modifier
-                        .width(70.dp)
-                        .height(50.dp)
+                Text(
+                    "Resultado: ${resultState.value}",
+                    textAlign = TextAlign.Center,
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-
-
-                Text("Resultado: ${resultState.value}")
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-
-                GradientButton(
+                Button(
                     onClick = {
-                        val result = calcularConsumo(consumoVeiculo, combustivel)
-                        resultState.value = result
+                    val result = calcularConsumo(consumoVeiculo, combustivel)
+                    resultState.value = result },
+                    shape = ShapeButton.large,
+                    colors = ButtonDefaults.buttonColors(RoxoClaro)
 
-                    }, text = "Calcular", color1 = Laranja, color2 = AmareloClaro, color3 = Marrom
-                )
+                ) {
+                    Text(
+                        text = "CALCULAR",
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .padding(10.dp)
+                    )
+
+                }
             }
 
         }
